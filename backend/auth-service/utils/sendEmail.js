44c7e8c889739992b8139e1fe1cf45cdb9298c85
Ayyash <ayyashzamny2002@gmail.com>
2025-04-28 23:44:00 +0530

    error logs for auth conteroller added

--- backend/auth-service/utils/sendEmail.js
+++ backend/auth-service/utils/sendEmail.js
@@ -3,19 +3,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sendEmail = async (to, subject, text) => {
+  // Create a transporter object using Gmail SMTP
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER,// Your Gmail address from .env
+      pass: process.env.EMAIL_PASS,// Your Gmail app password from .env
     },
   });
 
+  // Send the email with the specified details
   await transporter.sendMail({
-    from: `"Password Reset" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
+    from: `"Password Reset" <${process.env.EMAIL_USER}>`, // Sender's name and email
+    to,                                                    // Recipient
+    subject,                                               // Email subject
+    text,                                                  // Email plain text body
   });
 };
 
